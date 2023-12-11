a240c7f38c48a16a2da2a2c5a2707f7999630f40
Zia Amiri <zia.ro@live.com>
2023-12-11 03:36:11 +0000

    first commit

--- Main.java
+++ Main.java
@@ -2,29 +2,23 @@ import java.util.Scanner;
 
 class Main {
   public static void main(String[] args) {
-    String name; // To hold a name
-    int hours; //hours worked
-    double payRate; // Hourly pay rate
-    double grossPay; // Gross pay
-  // create scanner object to read input
+    String name;
+    int age;
+    double income;
     Scanner keyboard = new Scanner(System.in);
-    // Get the user's name.
-    System.out.print("What is your name? ");
+    // get the user age 
+    System.out.println("What's your age?");
+    age = keyboard.nextInt();
+    // get the user income
+    System.out.println("What is your income?");
+    income = keyboard.nextDouble();
+
+    // consume the new line 
+    keyboard.nextLine();
+    // get the user name
+    System.out.println("What is your name?");
     name = keyboard.nextLine();
-
-    //get the number of hours worked this week
-    System.out.println("How many hours did you work this week? ");
-    hours = keyboard.nextInt();
-
-    //get the number of hourly pay rate 
-    System.out.println("What is your hourly pay rate? ");
-    payRate = keyboard.nextDouble();
-
-    //calculate the gross pay
-    grossPay = hours * payRate;
-
-    // display the result information 
-    System.out.println("Hello, " + name);
-    System.out.println("Your gross pay is $" + grossPay);
+    // display the information back to the user
+    System.out.println("Hello, " + name + ". Your age is " + age + " and your income is " + income);
   }
 }
